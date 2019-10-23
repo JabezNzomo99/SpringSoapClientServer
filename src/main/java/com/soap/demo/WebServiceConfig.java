2018ce0be618fe4e0aa5852848c21c1390ca74d9
Anniekobia <kobiaannunziata94@gmail.com>
2019-10-23 14:10:40 +0300

    SOAP

---
+++ src/main/java/com/soap/demo/WebServiceConfig.java
@@ -0,0 +1,80 @@
+package com.soap.demo;
+
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.ws.config.annotation.EnableWs;
+import org.springframework.ws.config.annotation.WsConfigurerAdapter;
+import org.springframework.ws.transport.http.MessageDispatcherServlet;
+import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
+import org.springframework.xml.xsd.SimpleXsdSchema;
+import org.springframework.xml.xsd.XsdSchema;
+
+
+@EnableWs
+@Configuration
+public class WebServiceConfig extends WsConfigurerAdapter {
+    @Bean
+    public ServletRegistrationBean messageDispatcherServlet(ApplicationContext applicationContext) {
+        MessageDispatcherServlet servlet = new MessageDispatcherServlet();
+        servlet.setApplicationContext(applicationContext);
+        servlet.setTransformWsdlLocations(true);
+        return new ServletRegistrationBean(servlet, "/ws/*");
+    }
+
+
+    @Bean(name = "movies")
+    public DefaultWsdl11Definition defaultWsdl11Definition(XsdSchema moviesSchema) {
+        DefaultWsdl11Definition wsdl11Definition = new DefaultWsdl11Definition();
+        wsdl11Definition.setPortTypeName("MoviesPort");
+        wsdl11Definition.setLocationUri("/ws");
+        wsdl11Definition.setTargetNamespace("http://localhost:movie");
+        wsdl11Definition.setSchema(moviesSchema);
+        return wsdl11Definition;
+    }
+
+
+    @Bean
+    public XsdSchema moviesSchema() {
+        return new SimpleXsdSchema(new ClassPathResource("movies.xsd"));
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
